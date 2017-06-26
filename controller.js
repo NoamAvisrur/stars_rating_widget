962c6c928200ef0563f771e1b9fd3c2cbc659f6f
NoamAvisrur <noamavisrur@gmail.com>
2017-06-26 23:26:34 +0300

    added comments options

--- controller.js
+++ controller.js
@@ -8,8 +8,16 @@ myApp.filter('toFixed', function() {
     }    
 })
 
+myApp.filter('unsafe', function($sce) {
+    return function(val) {
+        return $sce.trustAsHtml(val);
+    };
+});
+
 myApp.controller('Ctrl', function Ctrl() {
     this.rates = [];
+
+    this.comments = [];
     
     this.ratesVals = {
             1: 0,
@@ -26,7 +34,7 @@ myApp.controller('Ctrl', function Ctrl() {
             localStorage.setItem('rating', JSON.stringify(this.rates));
             this.clearStars();
             this.renderView();
-            this.renderAverage();
+	    this.pushComments(selectedStars.length);
         }    
     }
     
@@ -42,6 +50,8 @@ myApp.controller('Ctrl', function Ctrl() {
             this.ratesVals[rate] = this.rates.filter(n => rate == n).length;
         });
         this.renderStats(this.ratesVals, this.rates);
+	this.renderAverage();
+        this.renderStarsAverage();
     }
     
     this.renderAverage = function(){
@@ -53,10 +63,36 @@ myApp.controller('Ctrl', function Ctrl() {
     
     this.renderStats = function(ob, arr){  
         Object.keys(this.ratesVals).forEach(function (rate) {
-			var value = ob[rate];
+	    var value = ob[rate];
             document.querySelector(`.reviews_stats_row:nth-child(${rate}) .reviews_stats_pass`).style.setProperty('--star-persentage', (value / arr.length * 100) + '%');
-		})
-    }  
+	})
+    }
+    
+    this.renderStarsAverage = function(){
+        roundedAverage = Math.round(this.average);
+        Averagestars = document.querySelectorAll('.average_star');
+	for (let i = 0; i < Averagestars.length; i++) {
+                Averagestars[i].innerHTML = "&#9734;";
+        }
+	for (let i = 0; i < roundedAverage; i++) {
+                Averagestars[i].innerHTML = "&#9733;";
+        }
+    }
+    
+    this.pushComments = function(rateVal){
+        var newComment = document.querySelector('input[type="text"]').value;
+        this.comments.unshift(
+            {
+	        comment: newComment,
+                rates: rateVal
+	    }
+	);
+        this.StarsObject = {};
+        for(let i = 0; i < rateVal; i++){
+            this.StarsObject[i] = "&#9733";
+        }
+	document.querySelector('input[type="text"]').value = ""; 
+    }
 })
 
 
